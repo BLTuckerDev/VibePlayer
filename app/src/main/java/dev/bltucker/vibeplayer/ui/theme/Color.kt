0fb588954d543e8c9cdab49acfc3a48b2d255a08
Brett Tucker <bretttucker@gmail.com>
2025-12-21 19:32:02 -0700

    Define custom color palette for VibePlayer theme

--- app/src/main/java/dev/bltucker/vibeplayer/ui/theme/Color.kt
+++ app/src/main/java/dev/bltucker/vibeplayer/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package dev.bltucker.vibeplayer.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val ButtonPrimary = Color(0xFFDE84FF)
+val ButtonPrimary30 = Color(0x4DDE84FF)
+val ButtonHover = Color(0x471A2735)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val TextPrimary = Color(0xFFFFFFFF)
+val TextSecondary = Color(0xFFA7BBD1)
+val TextDisabled = Color(0xFF4C647C)
+
+val SurfaceBG = Color(0xFF0A131D)
+
+val AccentColor = Color(0xFFF1FF95)
