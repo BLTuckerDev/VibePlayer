2f8fc3b9ca98808acb24a462e6bc7793f636efad
Brett Tucker <bretttucker@gmail.com>
2025-12-22 16:22:25 -0700

    Add Coroutine Dispatchers module

---
+++ app/src/main/java/dev/bltucker/vibeplayer/di/DispatchersModule.kt
@@ -0,0 +1,30 @@
+package dev.bltucker.vibeplayer.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class IoDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class UiDispatcher
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DispatchersModule {
+
+    @Provides
+    @IoDispatcher
+    fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @Provides
+    @UiDispatcher
+    fun provideUiDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
