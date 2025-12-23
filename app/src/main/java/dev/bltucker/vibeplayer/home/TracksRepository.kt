44b0756c78ae1b715dd221cb3eef885341db6439
Brett Tucker <bretttucker@gmail.com>
2025-12-22 17:15:38 -0700

    Introduce TracksRepository for database abstraction

---
+++ app/src/main/java/dev/bltucker/vibeplayer/home/TracksRepository.kt
@@ -0,0 +1,22 @@
+package dev.bltucker.vibeplayer.home
+
+import dev.bltucker.vibeplayer.db.TrackEntity
+import dev.bltucker.vibeplayer.db.TracksDao
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class TracksRepository @Inject constructor(
+    private val tracksDao: TracksDao
+) {
+    fun getTracks(): Flow<List<TrackEntity>> = tracksDao.getAllTracks()
+
+    suspend fun addTracks(tracks: List<TrackEntity>) = tracksDao.insertAll(tracks)
+
+    suspend fun deleteTrack(track: TrackEntity) = tracksDao.delete(track)
+
+    suspend fun getTracksBatch(limit: Int, offset: Int): List<TrackEntity> = tracksDao.getTracksBatch(limit, offset)
+
+    suspend fun getTrackCount(): Int = tracksDao.getTrackCount()
+}
