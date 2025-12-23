5fb4fbb83b5ad2ed9a2e4b88c565665737b27b65
Brett Tucker <bretttucker@gmail.com>
2025-12-22 19:40:31 -0700

    Disable Room schema export

--- app/src/main/java/dev/bltucker/vibeplayer/db/AppDatabase.kt
+++ app/src/main/java/dev/bltucker/vibeplayer/db/AppDatabase.kt
@@ -3,7 +3,7 @@ package dev.bltucker.vibeplayer.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [TrackEntity::class], version = 1, exportSchema = true)
+@Database(entities = [TrackEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun tracksDao(): TracksDao
 }
